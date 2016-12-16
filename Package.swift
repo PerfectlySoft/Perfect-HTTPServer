90352931be3525792eaeacec41092ec31b5b3ec5
Kyle Jessup <badthing@gmail.com>
2016-12-16 11:22:47 -0500

    Un-sync with 2.0.x

--- Package.swift
+++ Package.swift
@@ -25,6 +25,6 @@ let package = Package(
 		Target(name: "CHTTPParser", dependencies: []),
 		Target(name: "PerfectHTTPServer", dependencies: ["CHTTPParser"])
 	],
-	dependencies: [.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0)],
+	dependencies: [.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 1)],
 	exclude: []
 )
